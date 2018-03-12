a5d7a6d2a905f7da7b3039d36ee6d49d18c96357
robertmartin8 <martin.robertandrew@gmail.com>
2018-03-12 14:44:27 +0800

    Realised that there's no need to copy the file.

--- KindleClippings.py
+++ KindleClippings.py
@@ -1,36 +1,16 @@
 import re
 import os
-from shutil import copyfile
 
-# Change these as appropriate
-src = '/Volumes/Kindle/documents/My Clippings.txt'
-dst = '/Users/User/clippings.txt'
-
-copyfile(src, dst)
-
-# The clippings file
-filename = "clippings.txt"
-# Output directory
-dirname = "kindle_clippings"
-
-# Each clipping always consists of 5 lines:
-# - title line
-# - clipping info/metadata
-# - a blank line
-# - clipping text
-# - a divider made up of equals signs
-
-# Check the file exists
-if not os.path.isfile(filename):
-    print("ERROR: cannot find " + filename)
-    print("Please make sure it is in the same folder as this script.")
-    raise IOError
+# Change this depending on your computer / kindle version
+SOURCE_FILE = '/Volumes/Kindle/documents/My Clippings.txt'
 
 
 def remove_chars(s):
     """
-    Remove special characters from the string
+    This is a utility function that removes special characters from the string, so that it can 
+    become a valid filename. 
     :param s: input string
+    :return: the input string, stripped of special characters
     """
     # Replace colons with a hyphen so "A: B" becomes "A - B"
     s = re.sub(' *: *', ' - ', s)
@@ -45,52 +25,69 @@ def remove_chars(s):
     return s
 
 
-# Create the output directory if it doesn't exist
-if not os.path.exists(dirname):
-    os.makedirs(dirname)
+def parse_highlights(dirname="kindle_clippings"):
+    """
+    Each clipping always consists of 5 lines:
+    - title line
+    - clipping info/metadata
+    - a blank line
+    - clipping text
+    - a divider made up of equals signs
+    Thus we can parse the clippings, and organise them by book. 
+    :param dirname: the output directory where all of organised highlights will go
+    :return: organises kindle highlights by book .
+    """
+    # Check that the source file (on the kindle) exists
+    if not os.path.isfile(SOURCE_FILE):
+        print("ERROR: cannot find " + SOURCE_FILE)
+        print("Please change the path as needed")
+        raise IOError
 
-# The set of titles already processed
-output_files = set()
-title = ''
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
 
-# Open clippings textfile and read data in lines
-f = open(filename)
+    # This will keep track of the titles that we have already processed
+    output_files = set()
+    title = ""
 
-for highlight in f.read().split("=========="):
-    lines = highlight.split('\n')[1:]
+    # Open clippings textfile and read data in lines
+    with open(SOURCE_FILE, 'r') as f:
+        # Notice that individual highlights within My Clippings.txt are separated by ==========
+        for highlight in f.read().split("=========="):
+            # For each highlight, we split it into the lines
+            lines = highlight.split('\n')[1:]
 
-    # Don't try to write if we have no body
-    if len(lines) < 3 or lines[3] == '':
-        continue
+            # Don't try to write if we have no body
+            if len(lines) < 3 or lines[3] == '':
+                continue
 
-    # Set title and trim hex
-    title = lines[0]
-    if title[0] == '\ufeff':
-        title = title[1:]
+            # Set title and trim the hex character
+            title = lines[0]
+            if title[0] == '\ufeff':
+                title = title[1:]
 
-    # Remove characters and create path
-    outfile_name = remove_chars(title) + '.txt'
-    path = dirname + '/' + outfile_name
+            # Remove characters and create path
+            outfile_name = remove_chars(title) + '.txt'
+            path = dirname + '/' + outfile_name
 
-    # If we haven't seen title yet, set mode to write. Else, set to append.
-    if outfile_name not in (list(output_files) + os.listdir(dirname)):
-        mode = 'w'
-        output_files.add(outfile_name)
-    else:
-        # If the title exists, read it as text so that we won't append duplicates
-        mode = 'a'
-        with open(path, 'r') as textfile:
-            current_text = textfile.read()
+            # If we haven't seen title yet, set mode to write. Else, set to append.
+            if outfile_name not in (list(output_files) + os.listdir(dirname)):
+                mode = 'w'
+                output_files.add(outfile_name)
+            else:
+                # If the title exists, read it as text so that we won't append duplicates
+                mode = 'a'
+                with open(path, 'r') as textfile:
+                    current_text = textfile.read()
 
-    clipping_text = lines[3]
+            clipping_text = lines[3]
 
-    with open(path, mode) as outfile:
-        # Write out the the clippings text if it's not already there
-        if clipping_text not in current_text:
-            outfile.write("%s\n\n...\n\n" % clipping_text)
+            with open(path, mode) as outfile:
+                # Write out the the clippings text if it's not already there
+                if clipping_text not in current_text:
+                    outfile.write(f"{clipping_text}\n\n...\n\n")
 
-f.close()
-
-print("\nExported titles:\n")
-for i in output_files:
-    print("%s" % i)
+    print("\nExported titles:\n")
+    for i in output_files:
+        print(i)
